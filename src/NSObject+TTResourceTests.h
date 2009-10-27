bad689930f14a00a76d95cb36200f11bbee7879f
Justin Searls <searls@gmail.com>
2009-10-27 11:30:57 -0400

    Set up Three20, logic tests.

---
+++ src/NSObject+TTResourceTests.h
@@ -0,0 +1,18 @@
+//
+//  NSObject+TTResourceTests.h
+//  TTResource
+//
+//  Created by Justin Searls on 10/27/09.
+//  Copyright 2009 Justin Searls.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+
+@interface NSObject_TTResourceTests : SenTestCase {
+
+}
+
+- (void) testTruth;
+
+@end
---
+++ src/NSObject+TTResourceTests.m
@@ -0,0 +1,18 @@
+//
+//  NSObject+TTResourceTests.m
+//  TTResource
+//
+//  Created by Justin Searls on 10/27/09.
+//  Copyright 2009 Justin Searls.
+//
+
+#import "NSObject+TTResourceTests.h"
+
+
+@implementation NSObject_TTResourceTests
+
+- (void)testTruth {
+  STAssertTrue(TRUE,@"Truth should be true.");
+}
+
+@end
